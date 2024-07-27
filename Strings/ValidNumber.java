c3adb4ce706368942ae9a04daef4ab96701f5e46
RajdeepPatel <rajdeeppatel010@gmail.com>
2024-07-27 21:43:20 +0530

    day 13

---
+++ Strings/ValidNumber.java
@@ -0,0 +1,13 @@
+import java.util.regex.Pattern;
+
+public class ValidNumber {
+    public static boolean isNumber(String s) {
+        String regex = "^[+-]?(\\d+(\\.\\d*)?|\\.\\d+)([eE][+-]?\\d+)?$";
+        return Pattern.matches(regex,s);
+    }
+
+    public static void main(String[] args) {
+        String s = ".";
+        System.err.println(isNumber(s));
+    }
+}
