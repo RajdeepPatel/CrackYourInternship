8930e11fa9377765fcaa52a28357a845b4344444
RajdeepPatel <rajdeeppatel010@gmail.com>
2024-08-05 22:57:45 +0530

    day 22

---
+++ Linked List/LinkedListCycle.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/linked-list-cycle/
+public class LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        ListNode hare = head;
+        ListNode tortoise = head;
+        
+        while(hare != null && hare.next !=null){
+            hare = hare.next.next;
+            tortoise = tortoise.next;
+
+            if(hare == tortoise){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+    ListNode listNode = new ListNode(1,
+    new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+    boolean isCycle = hasCycle(listNode);
+    System.out.println(isCycle);
+    }
+}
