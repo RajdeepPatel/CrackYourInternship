047b64aff5901543bb79f6395fda7115ae55e561
RajdeepPatel <rajdeeppatel010@gmail.com>
2024-09-03 00:00:30 +0530

    MaxConsecutiveOnes

---
+++ Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+public class MaxConsecutiveOnes {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int cnt=0,max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+              cnt++;
+              if(max<cnt){
+                max=cnt;
+              }
+            }
+            else{
+                cnt=0;
+            }
+        }
+    return max;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,1,0,1,1,1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+}
--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -12,6 +12,6 @@ public class MissingNumber {
 
     public static void main(String[] args) {
         int[] nums={3,0,1};
-        System.err.println(missingNumber(nums));
+        System.out.println(missingNumber(nums));
     }
 }
