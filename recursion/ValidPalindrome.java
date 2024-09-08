48fceb14bd703604cb2832a70451bccdc51f630e
RajdeepPatel <rajdeeppatel010@gmail.com>
2024-09-08 23:58:20 +0530

    ValidPalindrome

---
+++ recursion/ValidPalindrome.java
@@ -0,0 +1,21 @@
+class ValidatePalindrome{
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        return checkPalindrome(s, 0, s.length() - 1);
+    }
+
+    private static boolean checkPalindrome(String s, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+        if (s.charAt(left) != s.charAt(right)) {
+            return false;
+        }
+        return checkPalindrome(s, left + 1, right - 1);
+    }
+    
+   public static void main(String...arg){
+	String s = "A man, a plan, a canal: Panama";
+	System.out.println(isPalindrome(s));
+   }
+}
